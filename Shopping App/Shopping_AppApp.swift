9947eb89d7bdb3715b526a9ca89007d9ddf6917d
mina eid <minaeid6@gmail.com>
2023-04-20 15:27:29 +0200

    Initial Commit

---
+++ Shopping App/Shopping_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Shopping_AppApp.swift
+//  Shopping App
+//
+//  Created by Mina on 20/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct Shopping_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
