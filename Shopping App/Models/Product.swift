2c7d18b8bbfb35a0ddbaf305f4508217c83f3388
mina eid <minaeid6@gmail.com>
2023-04-28 04:04:46 +0300

    Add

--- Shopping App/Models/Product.swift
+++ Shopping App/Models/Product.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+struct Product: Identifiable {
+    var id = UUID()
+    var name: String
+    var image: String
+    var price: Int
+}
+
+var productList = [ Product(name: "Orange sweater", image: "Sweater2", price: 54),
+                   Product(name: "Red wine sweater", image: "Sweater3", price: 89),
+                   Product(name: "Sand sweater", image: "Sweater6", price: 79),
+                   Product(name: "Sea sweater", image: "Sweater4", price: 94),
+                   Product(name: "Cream sweater", image: "Sweater5", price: 99),
+                   Product(name: "Beige sweater", image: "Sweater7", price: 65),
+                   Product(name: "Grey sweater", image: "Sweater1", price: 54),
+                   Product(name: "Mink sweater", image: "Sweater8", price: 83),
+]
--- Shopping App/Views/ContentView.swift
+++ Shopping App/Views/ContentView.swift
@@ -8,14 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var cartManager = CartManager()
+    var columns = [GridItem(.adaptive(minimum: 160) , spacing: 20)]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 20) {
+                    ForEach(productList, id: \.id) { product in
+                        ProductCard(product: product)
+                            .environmentObject(cartManager)
+                    }
+                }
+                .padding()
+            }
+            .navigationTitle(Text("Sweater Shop"))
+            .toolbar {
+                NavigationLink {
+                    CartView()
+                        .environmentObject(cartManager)
+                } label: {
+                    CartButton(numberOfProducts: cartManager.products.count)
+                }
+                
+            }
         }
-        .padding()
+        .navigationViewStyle(StackNavigationViewStyle())
     }
 }
 
