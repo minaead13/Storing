bbdae7c51a2c1711fa4dc3c9ce1889b2beaa2f0d
mina eid <minaeid6@gmail.com>
2023-04-28 04:04:14 +0300

    finished

---
+++ Shopping App/CartManager.swift
@@ -0,0 +1,8 @@
+//
+//  CartManager.swift
+//  Shopping App
+//
+//  Created by Mina on 20/04/2023.
+//
+
+import Foundation
---
+++ Shopping App/Components/PaymentButton.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentButton.swift
+//  Shopping App
+//
+//  Created by Mina on 20/04/2023.
+//
+
+import SwiftUI
+
+struct PaymentButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaymentButton_Previews: PreviewProvider {
+    static var previews: some View {
+        PaymentButton()
+    }
+}
